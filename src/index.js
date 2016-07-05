9a579b397e5510420b8e8ce1c82ba7ccb5b14ea2
Tsung Hung <hungmasterj@gmail.com>
2016-07-04 19:56:10 -0700

    added react router path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+require('styles/App.styl');
+
 import 'core-js/fn/object/assign';
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Navigation, browserHistory } from 'react-router';
 
-import StorePicker from './components/Main';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker />, document.getElementById('app'));
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={StorePicker}></Route>
+    <Route path="/store/:storeId" component={App}/>
+  </Router>
+), document.getElementById('app'));
