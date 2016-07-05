6c023936bbcc0eeb5a8395729619076c1a9e9499
Tsung Hung <hungmasterj@gmail.com>
2016-07-04 19:07:21 -0700

    removed unnecessary folders, updated favicon, added store picker, and main is the store picker

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,21 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.styl');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
-class AppComponent extends React.Component {
+class StorePicker extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <form className='store-selector'>
+        <h2>Type A Store Name</h2>
+        <input type='text' ref='storeId' required />
+        <input type='submit' className='btn' value='Submit' />
+      </form>
     );
   }
 }
 
-AppComponent.defaultProps = {
+StorePicker.defaultProps = {
 };
 
-export default AppComponent;
+export default StorePicker;
