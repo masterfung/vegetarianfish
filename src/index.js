1458e55bbda73f613b39af741a05382c785eb8a5
Tsung Hung <hungmasterj@gmail.com>
2016-07-05 01:21:13 -0700

    added nomatch and also name for store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ require('styles/App.styl');
 import 'core-js/fn/object/assign';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, Navigation, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import StorePicker from './components/StorePicker';
+import NoMatch from './components/NoMatch';
 import App from './components/App';
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={StorePicker}></Route>
-    <Route path="/store/:storeId" component={App}/>
+    <Route name='storePath' path="/store/:storeId" component={App}/>
+    <Route path="*" component={NoMatch}/>
   </Router>
 ), document.getElementById('app'));
