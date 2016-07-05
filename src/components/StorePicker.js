9cbabe55175bf380b6ce48bb19c34d56108790ae
Tsung Hung <hungmasterj@gmail.com>
2016-07-05 16:58:05 -0700

    added helper text and labels

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import { browserHistory, Navigation } from 'react-router';
 import h from '../scripts/helpers';
+import reactMixin from 'react-mixin';
 
 export default class StorePicker extends React.Component {
 
@@ -9,13 +10,16 @@ export default class StorePicker extends React.Component {
     event.preventDefault();
     const storeID = this.refs.storeID.value;
 
-    browserHistory.push('/store/' + storeID);
+    browserHistory.push(`/store/${storeID}`);
   }
 
   render() {
     return (
       <form className='store-selector' onSubmit={this.goToStoreName.bind(this)}>
         <h2 className='store-greeter'>Type A Store Name</h2>
+        <p className='welcome-text'>Welcome to the freshest Veggie Fish Market <br />
+           where you will find all your fishy desires fulfilled at an affordable price.</p>
+        <label id='store-label'>Store Name</label>
         <input type='text' ref='storeID' defaultValue={h.getFunName()} required />
         <input type='submit'
          className='btn'
