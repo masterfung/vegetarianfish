d0f96f08fba6bc49338ec529f6f195640b174524
Tsung Hung <hungmasterj@gmail.com>
2016-07-05 16:57:50 -0700

    complete working progress of the ordering for veggie fishes

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import h from '../scripts/helpers';
 
 export default class Order extends React.Component {
-  render() {
+  renderOrder(key) {
+    let fish = this.props.fishes[key];
+    let count = this.props.order[key];
+
+    if (!fish) {
+      return (
+        <li key={key}>
+          Sorry, this veggie {`${fish}`} is gone.
+        </li>
+      )
+    }
+
     return (
-      <div>
-        <h2>Order</h2>
+      <li key={key}>
+        {count}lbs
+        {fish.name}
+        <span className='price'>{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  }
+
+  render() {
+
+    let orderIDsArray = Object.keys(this.props.order);
+
+    let totalAmount = orderIDsArray.reduce((prevTotal, key) => {
+      let count = this.props.order[key],
+          fish = this.props.fishes[key],
+          isAvailable = fish && fish.status === 'available';
+
+
+      if (fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className='order-wrap'>
+        <h2 className='order-title'>Basket</h2>
+        <ul className='order'>
+          {orderIDsArray.map(this.renderOrder.bind(this))}
+          <li className='total'>
+            <strong>Total:</strong>
+            {h.formatPrice(totalAmount)}
+          </li>
+        </ul>
       </div>
     )
   }
