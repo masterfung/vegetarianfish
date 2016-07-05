efb56d8b5d71a11d9ac6db3ee85fad795c4e0f73
Tsung Hung <hungmasterj@gmail.com>
2016-07-05 16:58:59 -0700

    added firebase, mixins, many funcs to render and orders and component lifescycle things with localstorage and firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import uuid from 'node-uuid';
+import Rebase from 're-base';
+import Catalyst from 'react-catalyst';
+import reactMixin from 'react-mixin';
 
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import FishItem from './FishItem';
 
-export default class App extends React.Component {
+let base = Rebase.createClass('https://veggie-fish.firebaseio.com/');
+
+base.authWith
+
+class App extends React.Component {
+  static propTypes = {
+
+  }
+
   constructor(props) {
     super(props)
     this.state = {
       fishes: {},
       order: {}
     }
+  }
+
+  componentDidMount() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context: this,
+      state: 'fishes'
+    });
+
+    let localStorageRef = localStorage.getItem('order-'+this.props.params.storeId);
+
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem('order-'+ this.props.params.storeId, JSON.stringify(nextState.order))
   }
 
   addFish(fish) {
@@ -21,8 +52,22 @@ export default class App extends React.Component {
     this.setState({ fishes: this.state.fishes });
   }
 
+  addToOrder(key) {
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({order: this.state.order});
+  }
+
   loadFish() {
     this.setState({fishes: require('../scripts/fish_samples.js')})
+  }
+
+  renderFish(key) {
+    return <FishItem
+             key={key}
+             index={key}
+             details={this.state.fishes[key]}
+             addToOrder={this.addToOrder.bind(this)}
+              />
   }
 
   render() {
@@ -30,8 +75,11 @@ export default class App extends React.Component {
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Vegetarian Seafood Market' />
+          <ul className='list-of-fishes'>
+            {Object.keys(this.state.fishes).map(this.renderFish.bind(this))}
+          </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish.bind(this)}
           loadFish={this.loadFish.bind(this)}
@@ -40,3 +88,8 @@ export default class App extends React.Component {
     )
   }
 }
+
+reactMixin.onClass(App, Catalyst.LinkedStateMixin);
+
+
+export default App;
