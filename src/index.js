92c4d06f306de97f2c4887d8dd78816960e3534c
Tsung Hung <hungmasterj@gmail.com>
2016-07-04 18:09:36 -0700

    first commit;

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/Main';
+
+// Render the main component into the dom
+render(<App />, document.getElementById('app'));
