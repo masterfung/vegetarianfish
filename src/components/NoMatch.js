7d8fb2c7e5c4afbb2044e6011645520205046374
Tsung Hung <hungmasterj@gmail.com>
2016-07-04 21:53:23 -0700

    added no match for all 404

---
+++ src/components/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NoMatch extends React.Component {
+  render() {
+    return (
+      <h1 className='not-found'>
+        Path Not Found!
+      </h1>
+    )
+  }
+}
